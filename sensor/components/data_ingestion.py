eee77002f5218445b2a5fe15d162496aa08a0303
Rakesh Pawar <pawarrrakesh08@gmail.com>
2023-09-09 19:34:40 +0530

    completed the data_ingestion

---
+++ sensor/components/data_ingestion.py
@@ -0,0 +1,61 @@
+from sensor import utils
+from sensor.entity import config_entity
+from sensor.entity import artifact_entity
+from sensor.exception import CustomException
+from sensor.logger import logging
+import os,sys
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+Class DataIngestion:
+    
+    def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig):
+        try:
+            self.data_ingestion_config = data_ingestion_config
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def initiate_data_ingestion(self)->artifact_entity.DataIngestionArtifact:
+        try:
+            logging.info(f"Exporting collection data as pandas dataframe")
+            df:pd.DataFrame = utils.get_collection_as_dataframe(
+                database_name=self.data_ingestion_config.database_name,
+                colloection_name=self.data_ingestion_config.collection_name
+            )
+            
+            logging.info("Save data in feature store")
+            df.replace(to_replace="na",value=np.NAN,inplace=True)
+            
+            logging.info("Create feature store folder if not available")
+            feature_store_dir = os.path.dirname(self.data_ingestion_config.feature_store_file_path)
+            os.makedirs(feature_store_dir,exist_ok=True)
+            
+            logging.info("Save the df to feature store folder")
+            
+            df.to_csv(path_or_buf=self.data_ingestion_config.feature_store_file_path,index=False,header=True)
+            
+            logging.info("split the dataset into train and test")
+            
+            train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.train_file_path)
+            os.makedirs(dataset_dir,exist_ok=True)
+            
+            logging.info("Save the df to feature store folder")
+            
+            train_df.to_csv(path_or_buf = self.data_ingestion_config.train_file_path,index=False,header=True)
+            test_df.to_csv(path_or_buf=self.data_ingestion_config.test_file_path,index=False,header=True)
+            
+            
+            data_ingestion_aritifact = artifact_entity.DataIngestionArtifact(
+                feature_store_file_path = self.data_ingestion_config.feature_store_file_path,
+                train_file_path = self.data_ingestion_config.train_file_path,
+                test_file_path = self.data_ingestion_config.test_file_path
+            )
+            
+            logging.info(f"Data Ingestion artifact: {data_ingestion_aritifact}")
+            return data_ingestion_aritifact
+        except Exception as e:
+            raise CustomException(error_message=e,error_detail=sys)
+            
+
